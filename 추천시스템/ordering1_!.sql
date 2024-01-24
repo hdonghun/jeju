eade92f1b7450cd32435e2f46463136992a10f9c
cindy8946 <cindy8946@gmail.com>
2024-01-24 13:35:15 +0900

    ordering

---
+++ 추천시스템/ordering1_!.sql
@@ -0,0 +1,5 @@
+CREATE TABLE jeju_bjh_over_2 AS
+SELECT * FROM tourist_destination.jeju_dataset_bjh
+ORDER BY 오버투어리즘 ASC
+LIMIT 200;
+
---
+++ 추천시스템/ordering1_2.sql
@@ -0,0 +1,4 @@
+
+CREATE TABLE jeju_bjh_ratio_2 AS
+SELECT * FROM tourist_destination.jeju_bjh_over_2
+ORDER BY ratio DESC;
